e6334855cba21b8d8a67c588a8012e28868cd0dd
Josiah Berkebile <praenato14@gmail.com>
2017-07-30 15:07:42 -0500

    proper sublime project and use tabs, not spaces

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 var _ = require('lodash');
 
 module.exports.loop = function () {
-  if (Game.creeps.length < 1) {
-    console.log("creating creep")
-    var creepName = Game.spawns["Spawn1"].createCreep([MOVE, WORK, CARRY], "creep0");
-    console.log(creepName);
-    console.log();
-  } else {
-    console.log(Game.creeps["creep0"]);
-  }
+	if (Game.creeps.length < 1) {
+		console.log("creating creep")
+		var creepName = Game.spawns["Spawn1"].createCreep([MOVE, WORK, CARRY], "creep0");
+		console.log(creepName);
+		console.log();
+	} else {
+		console.log(Game.creeps["creep0"]);
+	}
 };
